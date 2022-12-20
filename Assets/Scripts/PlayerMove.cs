7f96e11b900942b2346f607431130302e17d2ff6
알파 <arpa1705@gmail.com>
2022-12-20 10:19:59 +0900

    feat: 플레이어 움직임 개선

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,33 +5,91 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float maxJumpSpeed;
+    public Sprite[] sprites;
     Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    bool isJumping;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        isJumping = false;
     }
 
-    void Update()
+    void ResetSprite()
     {
-        // Limit max speed
-        if (Mathf.Abs(rigidBody.velocity.x) > maxSpeed)
+        spriteRenderer.sprite = sprites[1];
+    }
+
+    void MoveHorizontalUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float direction = h == 0 ? rigidBody.velocity.normalized.x : h;
+
+        animator.enabled = false;
+
+        // Stop when both arrow key down
+        if (h == 0)
         {
-            rigidBody.velocity = new Vector2(maxSpeed * rigidBody.velocity.normalized.x, rigidBody.velocity.y);
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            ResetSprite();
         }
-        // move
         else
         {
-            float h = Input.GetAxisRaw("Horizontal");
-
-            rigidBody.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+            // Limit max speed
+            if (Mathf.Abs(rigidBody.velocity.x) > maxSpeed)
+            {
+                rigidBody.velocity = new Vector2(maxSpeed * direction, rigidBody.velocity.y);
+                animator.enabled = true;
+            }
+            // move
+            else
+            {
+                animator.enabled = true;
+                rigidBody.AddForce(Vector2.right * h * 2f, ForceMode2D.Impulse);
+            }
         }
 
         // Stop when button up
         if (Input.GetButtonUp("Horizontal"))
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.normalized.x * 0.5f, rigidBody.velocity.y);
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            animator.enabled = false;
+            ResetSprite();
         }
     }
 
+    void FlipUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float direction = h == 0 ? rigidBody.velocity.normalized.x : h;
+
+        // Flip
+        if (h != 0)
+        {
+            spriteRenderer.flipX = direction == -1;
+        }
+    }
+
+    void Update()
+    {
+        FlipUpdate();
+    }
+
+
+    void FixedUpdate()
+    {
+        MoveHorizontalUpdate();
+
+        if (isJumping && Input.GetAxisRaw("Jump") == 1)
+        {
+            isJumping = true;
+            rigidBody.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+
+        }
+    }
 }
